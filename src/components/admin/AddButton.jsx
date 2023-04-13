04aa950f4af7197a0991375b9148c3057d21e85c
Sulekha Jamaluddin <sulu.lekha@gmail.com>
2023-04-13 07:36:01 +0200

    Fixed add episode issue

--- src/components/admin/AddButton.jsx
+++ src/components/admin/AddButton.jsx
@@ -3,7 +3,8 @@ import titleData from "../../data/titleData.json";
 import titleFields from "../../data/createTitleFields.json";
 import episodeData from "../../data/episodeData.json";
 import episodeFields from "../../data/createEpisodeFields.json";
-import FormCreate from "../../components/admin/form/FormCreate";
+import FormCreateTitle from "../../components/admin/form/FormCreate";
+import FormCreateEpisode from "./form/FormCreateEpisode";
 import { useModal } from "../../state/ModalProvider";
 
 export default function AddButton({ path, type, text, state }) {
@@ -12,15 +13,18 @@ export default function AddButton({ path, type, text, state }) {
   const data =
     text === "title" ? { ...titleData, category: type } : episodeData;
 
-  const CreateForm = (
-    <FormCreate
-      path={path}
-      fields={fields}
-      data={data}
-      text={text}
-      state={state}
-    />
-  );
+  const CreateForm =
+    text === "title" ? (
+      <FormCreateTitle path={path} fields={fields} data={data} text={text} />
+    ) : (
+      <FormCreateEpisode
+        path={path}
+        fields={fields}
+        data={data}
+        text={text}
+        state={state}
+      />
+    );
 
   return (
     <button className="add-btn" onClick={() => openModal(CreateForm)}>
--- src/components/admin/form/FormCreateEpisode.jsx
+++ src/components/admin/form/FormCreateEpisode.jsx
@@ -5,37 +5,20 @@ import { useState } from "react";
 import FormFieldGenerator from "../../common/form/FormFieldGenerator";
 import { createDocument } from "../../../scripts/firestore/createDocument";
 import { useModal } from "../../../state/ModalProvider";
-import { useTitles } from "../../../state/TitlesProvider";
 
-export default function FormCreate({ path, fields, data, text }) {
+export default function FormCreateEpisode({ path, fields, data, text, state }) {
   // Global state
   const { closeModal } = useModal();
-  const { titlesDispatch, seasonsDispatch } = useTitles();
+  const [episodes, setEpisodes] = state;
 
   // Local state
   const [form, setForm] = useState(data);
 
-  //Methods
-  async function createFirstSeason(newSeriesId) {
-    const firstSeasonPath = `${path}/${newSeriesId}/seasons`;
-    const firstSeason = {
-      seasonNumber: 1,
-    };
-    const documentId = await createDocument(firstSeasonPath, firstSeason);
-    seasonsDispatch({
-      type: "create",
-      payload: { ...firstSeason, id: documentId },
-    });
-  }
-
   async function onSubmit(event) {
     event.preventDefault();
     const documentId = await createDocument(path, form);
-    titlesDispatch({
-      type: "create",
-      payload: { id: documentId, ...form },
-    });
-    data.category === "series" && createFirstSeason(documentId);
+    const newEpisode = { id: documentId, ...form };
+    setEpisodes([...episodes, newEpisode]);
     closeModal();
   }
 
